5ef8def90857b58518756b86afc820e68f361a0b
fentybit <fentymhall@gmail.com>
2021-09-13 15:17:49 -0700

    multiple pointers pattern

--- problem_solving_patterns.js
+++ problem_solving_patterns.js
@@ -10,8 +10,10 @@ validAnagram('awesome', 'awesom') // false
 validAnagram('qwerty', 'qeywrt') // true
 validAnagram('texttwisttime', 'timetwisttext') // true
 
-// Note: You may assume the string contains only lowercase alphabets.
 // Time Complexity O(n)
+
+// Note: 
+// You may assume the string contains only lowercase alphabets
 
 function validAnagram(word1, word2) {
   if (word1.length !== word2.length) return false;
@@ -57,3 +59,52 @@ function validAnagram(first, second) {
     return true;
   }
 }
+
+//! Multiple Pointers
+
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-1, -1, -1, 0, 1]) // 4
+
+// Time Complexity O(n)
+// Space Complexity O(n)
+
+// Bonus: 
+// You must do this with constant or O(1) space and O(n) time
+
+// Solution with both O(n) time and space complexity
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  let value;
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    value = arr[i];
+
+    if (!newArr.includes(value)) {
+      newArr.push(value);
+    }
+  }
+
+  return newArr.length;
+}
+
+// Solution with O(1) space and O(n) time
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+//! Sliding Window Pattern
