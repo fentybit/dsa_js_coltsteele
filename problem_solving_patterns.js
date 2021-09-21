0a99f6400999e91fb5241c70a7069c32e74c565f
fentybit <fentymhall@gmail.com>
2021-09-21 10:46:00 -0700

    minSubArrayLen with sliding window pattern

--- problem_solving_patterns.js
+++ problem_solving_patterns.js
@@ -303,7 +303,42 @@ function maxSubarraySum(arr, num) {
   return maxSum;
 }
 
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive
+// integers and a positive integer.
+// This function should return the minimal lengh of a contiguous subarray of which the sum is 
+// greater than or equal to the integer passed to the function. If there isn't one, return 0 
+// instead.
 
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7) // 2
+minSubArrayLen([2, 1, 6, 5, 4], 9) // 2
+minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) // 1
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39) // 3
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55) // 5
+minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) // 0
+
+// O(n) time complexity and O(1) space complexity
+function minSubArrayLen(arr, sum) {
+  let start = 0;
+  let end = 0;
+  let total = 0
+  let minLen = Infinity;
+
+  while (start < arr.length) {
+    if (total < sum && end < arr.length) {
+      total += arr[end];
+      end++;
+    } else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= arr[start];
+      start++;
+    } else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
 
 //! Divide and Conquer
 // This pattern involves dividing a data set into smaller chunks and then repeating a process 
