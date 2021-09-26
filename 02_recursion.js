eec4e1dab12869c8860e2f4620478a2c06213442
fentybit <fentymhall@gmail.com>
2021-09-26 12:11:39 -0700

    recursions on recursiveRange, fib and reverse

--- 02_recursion.js
+++ 02_recursion.js
@@ -10,8 +10,7 @@ power(2, 4) // 16
 
 function power(base, exp) {
   if (exp === 0) return 1;
-
-  if (exp > 0) return base * power(base, exp - 1);
+  return base * power(base, exp - 1);
 }
 
 // Write a function factorial which accepts a number and returns the factorial of that number.
@@ -25,8 +24,7 @@ factorial(7) // 5040
 
 function factorial(num) {
   if (num === 0) return 1;
-
-  if (num > 0) return num * factorial(num - 1);
+  return num * factorial(num - 1);
 }
 
 // Write a function called productOfArray which takes in an array of numbers and returns the product
@@ -37,8 +35,7 @@ productOfArray([1, 2, 3, 10]) // 60
 
 function productOfArray(arr) {
   if (arr.length === 1) return arr[0];
-
-  if (arr.length > 0) return arr[0] * productOfArray(arr.slice(1));
+  return arr[0] * productOfArray(arr.slice(1));
 }
 
 // Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 
@@ -48,5 +45,44 @@ recursiveRange(6) // 21
 recursiveRange(10) // 55
 
 function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+// Write a recursive function called fib which accepts a number and returns the nth number in 
+// the Fibonacci sequence. Recall that Fibonacci sequence is the sequence of the whole numbers
+// 1, 1, 2, 3, 5, 8, ...which starts with 1 and 1, and where every number thereafter is equal
+// to the sum of the previous two numbers.
+
+fib(4) // 3
+fib(10) // 55
+fib(28) // 317811
+fib(35) // 9227465
+
+function fib(num) {
+  if (num <= 2) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
+
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+reverse('awesome') // 'emosewa'
+reverse('rithmschool') // 'loohcsmhtir'
+
+function reverse(str) {
+  if (str.length === 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+// Write a recursive function called isPalindrome which returns true if the string passed to it is a
+// palindrome (reads the same forward and backward). Otherwise it returns false.
+
+isPalindrome('awesome') // false
+isPalindrome('foobar') // false
+isPalindrome('tacocat') // true
+isPalindrome('amanaplanacanalpanama') // true
+isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str) {
 
 }
