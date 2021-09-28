e8ccb3af1c74ee43be7b05d06e44b194b18dd093
fentybit <fentymhall@gmail.com>
2021-09-28 06:59:32 -0700

    recursions on flatten

--- 02_recursion.js
+++ 02_recursion.js
@@ -105,3 +105,33 @@ function someRecursive(arr, callback) {
   return someRecursive(arr.slice(1), callback);
 }
 
+// Write a recursive function called flatten which accepts an array of arrays and returns a new array
+// with all values flattened.
+
+flatten([1, 2, 3, [4, 5]]) // [1, 2, 3, 4, 5]
+flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+flatten([[1], [2], [3]]) // [1,2,3]
+flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
+
+function flatten(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArr = newArr.concat(flatten(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+// Write a recursive function called capitalizeFirst.
+// Given an array of strings, capitalize the first letter of each string in the array.
+
+capitalizeFirst(['car', 'taco', 'banana']); // ['Car','Taco','Banana']
+
+function capitalizeFirst(arr) {
+
+}
