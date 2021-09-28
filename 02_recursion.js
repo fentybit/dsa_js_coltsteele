6a62e0a3f31b13acd72c4832d92e499fbe2c5218
fentybit <fentymhall@gmail.com>
2021-09-28 06:55:52 -0700

    recursions on isPalindrome and someRecursive

--- 02_recursion.js
+++ 02_recursion.js
@@ -84,5 +84,24 @@ isPalindrome('amanaplanacanalpanama') // true
 isPalindrome('amanaplanacanalpandemonium') // false
 
 function isPalindrome(str) {
+  if (str.length === 0) return true;
+  if (str[0] === str[str.length - 1]) return isPalindrome(str.slice(1, str.length - 1));
 
+  return false;
 }
+
+// Write a recursive function called someRecursive which accepts an array and a callback. The function 
+// returns true if a single value in the array returns true when passed to the callback. Otherwise,
+// it returns false.
+
+someRecursive([1, 2, 3, 4], isOdd) // true
+someRecursive([4, 6, 8, 9], isOdd) // true
+someRecursive([4, 6, 8], isOdd) // false
+someRecursive([4, 6, 8], val => val > 10); // false
+
+function someRecursive(arr, callback) {
+  if (arr.length === 0) return false;
+  if (callback(arr[0])) return true;
+  return someRecursive(arr.slice(1), callback);
+}
+
