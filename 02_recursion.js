22e4264bf2edd15706e8df9063f8e295931afa3e
fentybit <fentymhall@gmail.com>
2021-09-28 19:05:41 -0700

    recursions on capitalizeFirst

--- 02_recursion.js
+++ 02_recursion.js
@@ -132,6 +132,22 @@ function flatten(arr) {
 
 capitalizeFirst(['car', 'taco', 'banana']); // ['Car','Taco','Banana']
 
+// Non-recursive solution
 function capitalizeFirst(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1))
+  }
+  return newArr;
+}
 
+// Recursive solution
+function capitalizeFirst(arr) {
+  if (arr.length === 1) return [arr[0][0].toUpperCase() + arr[0].slice(1)];
+
+  let newArr = capitalizeFirst(arr.slice(0, -1));
+  let string = arr.slice(arr.length - 1)[0][0].toUpperCase() + arr.slice(arr.length - 1)[0].substring(1);
+  newArr.push(string);
+
+  return newArr;
 }
