f0f223a4f968be33c1cc357b156b607c168d75b7
fentybit <fentymhall@gmail.com>
2021-10-02 19:31:32 -0700

    recursion on stringifyNumbers

--- 02_recursion.js
+++ 02_recursion.js
@@ -205,3 +205,47 @@ function capitalizeWords(arr) {
   return recursion;
 }
 
+// Write a function called stringifyNumbers which takes in an object and finds all of the values
+// which are numbers and converts them to strings. 
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66
+    }
+  }
+}
+
+stringifyNumbers(obj)
+// {
+//  num: "1",
+//  test: [],
+//  data: {
+//    val: "4",
+//    info: {
+//      isRight: true,
+//      random: "66"
+//    }
+//  }
+// }
+
+function stringifyNumbers(obj) {
+  let newObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
+
