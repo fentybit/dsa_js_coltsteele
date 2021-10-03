d3915869bba53a2c019c3e3a18d9581a4a3c1650
fentybit <fentymhall@gmail.com>
2021-10-03 07:49:23 -0700

    recursion on collectStrings

--- 02_recursion.js
+++ 02_recursion.js
@@ -249,3 +249,69 @@ function stringifyNumbers(obj) {
   return newObj;
 }
 
+// Write a function called collectStrings which accepts an object and returns an array of all 
+// the values in the object that have a typeof string.
+
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz"
+          }
+        }
+      }
+    }
+  }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
+
+// Helper method recursion solution
+function collectStrings(obj) {
+  let arr = [];
+
+  function gatherStrings(o) {
+    for (let key in o) {
+      if (typeof o[key] === 'string') {
+        arr.push(o[key]);
+      } else if (typeof o[key] === 'object') {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+  return arr;
+}
+
+// Pure recursion solution
+function collectStrings(obj) {
+  let arr = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      arr.push(obj[key]);
+    } else if (typeof obj[key] === 'object') {
+      arr = arr.concat(collecStrings(obj[key]));
+    }
+  }
+
+  return arr;
+}
+
+// Similar solution to pure recursion but adding 'arr' to argument as []
+function collectStrings(obj, arr = []) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'string') {
+      arr.push(obj[key]);
+    } else if (typeof obj[key] === 'object') {
+      collectStrings(obj[key], arr)
+    }
+  }
+
+  return arr;
+}
