a075e811c52c3901e6baab845128564e462b87c3
fentybit <fentymhall@gmail.com>
2021-10-04 07:16:54 -0700

    search algorithms on naiveSearch

--- 03_searching_algorithms.js
+++ 03_searching_algorithms.js
@@ -47,3 +47,18 @@ function binarySearch(arr, value) {
   return -1;
 }
 
+// Supposed you want to count the number of times a smaller string appears in a longer string.
+// Avoid straighforward approach involving checking pairs of characters individually.
+
+naiveSearch("lorie loled", "lol") // 1
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
