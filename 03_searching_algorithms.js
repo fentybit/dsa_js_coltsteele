a012f117f200432cf10836bd72a88584a416e2e0
fentybit <fentymhall@gmail.com>
2021-10-04 07:16:33 -0700

    searching algorithms on binarySearch

--- 03_searching_algorithms.js
+++ 03_searching_algorithms.js
@@ -17,4 +17,33 @@ function linearSearch(arr, value) {
   return -1;
 }
 
+//! Binary Search 
+// Write a function called binarySearch which accepts a sorted array and a value and returns 
+// the index at which the value exists. Otherwise, return -1.
+// This algorithm should be more efficient than linearSearch.
+
+binarySearch([1, 2, 3, 4, 5], 2) // 1
+binarySearch([1, 2, 3, 4, 5], 6) // -1
+binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95) // 16
+binarySearch([5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100) // -1
+
+function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle;
+
+  while (left <= right) {
+    middle = Math.floor((right + left) / 2);
+
+    if (value === arr[middle]) {
+      return middle;
+    } else if (value < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+
+  return -1;
+}
 
