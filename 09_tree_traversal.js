dfdd25f5fa22dc56fefd458dc8439c666d1e24a2
fentybit <fentymhall@gmail.com>
2021-11-04 20:29:06 -0700

    depth first search preorder, postorder and inorder

--- 09_tree_traversal.js
+++ 09_tree_traversal.js
@@ -23,4 +23,41 @@ class TreeTraversal {
     return data;
   }
 
+  depthFirstSearchPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  depthFirstSearchPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  // this is commonly used with Binary Search Trees 
+  // since all nodes in the tree in their underlying order
+  depthFirstSearchInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
