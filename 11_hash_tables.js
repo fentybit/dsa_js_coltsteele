687cb825a12b446c1721c669294e3050f6156b20
fentybit <fentymhall@gmail.com>
2021-11-17 07:33:19 -0800

    hash tables on keys and values

--- 11_hash_tables.js
+++ 11_hash_tables.js
@@ -36,4 +36,34 @@ class HashTable {
 
     return undefined;
   }
+
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+
+    return keysArr;
+  }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+
+    return valuesArr;
+  }
 }
