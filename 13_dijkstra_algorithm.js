53ec5926a4e759c148a32f694c973b6f66251757
fentybit <fentymhall@gmail.com>
2021-11-25 15:27:02 -0800

    dijkstra algorithm on weighted graph

--- 13_dijkstra_algorithm.js
+++ 13_dijkstra_algorithm.js
@@ -19,3 +19,17 @@ class PriorityQueue {
   }
 }
 
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
