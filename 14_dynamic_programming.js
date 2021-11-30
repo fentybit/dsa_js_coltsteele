35ff96355beafe7e5150106169eba019bf48993b
fentybit <fentymhall@gmail.com>
2021-11-29 18:41:18 -0800

    dynamic programming fibonacci with memoization

--- 14_dynamic_programming.js
+++ 14_dynamic_programming.js
@@ -5,3 +5,12 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 // Time complexity is O(2^n)
+
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+// Time complexity is O(n)
