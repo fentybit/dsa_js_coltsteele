6eec4ccf792ee56d5666994c62e20eb1da05b631
fentybit <fentymhall@gmail.com>
2021-12-07 08:00:41 -0800

    divide and conquer on countZeroes

--- 01_problem_solving_patterns.js
+++ 01_problem_solving_patterns.js
@@ -424,3 +424,30 @@ function search(array, val) {
     return -1;
   }
 }
+
+// Given an array of 1s and 0s which has all 1st first followed by all 0s, write a function
+// called countZeroes, which returns the number of zeroes in the array.
+
+countZeroes([1, 1, 1, 1, 0, 0]) // 2
+countZeroes([1, 0, 0, 0, 0]) // 4
+countZeroes([0, 0, 0]) // 3
+countZeroes([1, 1, 1, 1]) // 0
+
+// Time Complexity O(log n)
+function countZeroes(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    let currentElement = arr[middle];
+
+    if (currentElement === 1) {
+      left = middle + 1;
+    } else if (currentElement === 0) {
+      right = middle - 1;
+    }
+  }
+
+  return arr.length - left;
+}
