c78707458df416c52dc95856384bd01967812686
fentybit <fentymhall@gmail.com>
2021-12-11 07:54:32 -0800

    bubble sort with comparator function

--- 04_bubble_selection_insertion_sort.js
+++ 04_bubble_selection_insertion_sort.js
@@ -53,6 +53,87 @@ function bubbleSort(arr) {
   return arr;
 }
 
+// Implement a function called bubbleSort. Given an array, bubbleSort will sort the 
+// values in the array. The function takes 2 parameters: an array and an optional 
+// comparator function.
+// The comparator function is a callback that will take two values from the array 
+// to be compared. The function returns a negative value if the first value is less
+// than the second, a positive value if the first value is greater than the second,
+// and 0 if both values are equal. The default comparator you provide should assume
+// that the two parameters are numbers and that we are sorting the values from 
+// smallest to largest.
+
+bubbleSort([4, 20, 12, 10, 7, 9]) // [4, 7, 9, 10, 12, 20]
+bubbleSort([0, -10, 7, 4]) // [-10, 0, 4, 7]
+bubbleSort([1, 2, 3]) // [1, 2, 3]
+bubbleSort([])
+
+let kitties = ['LilBub', 'Garfield', 'Heathcliff', 'Blue', 'Grumpy']
+function strComp(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+bubbleSort(kitties, strComp) // ['Blue', 'Garfield', 'Grumpy', 'Heathcliff', LilBub']
+
+let moarKittyDate = [{
+  name: 'LilBub',
+  age: 7
+}, {
+  name: 'Garfield',
+  age: 40
+}, {
+  name: 'Heathcliff',
+  age: 45
+}, {
+  name: 'Blue',
+  age: 1
+}, {
+  name: 'Grumpy',
+  age: 6
+}]
+function oldestToYoungest(a, b) {
+  return b.age - a.age;
+}
+
+function bubbleSort(arr, comparator) {
+  let noSwap;
+  const swap = (arr, idx1, idx2) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+  if (typeof comparator !== 'function') {
+    for (let i = arr.length; i > 0; i--) {
+      noSwap = true;
+
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          noSwap = false;
+        }
+      }
+
+      if (noSwap) break;
+    }
+  } else {
+    for (let i = arr.length; i > 0; i--) {
+      noSwap = true;
+
+      for (let j = 0; j < i - 1; j++) {
+        if (comparator(arr[j], arr[j + 1]) >= 1) {
+          swap(arr, j, j + 1);
+          noSwap = false;
+        }
+      }
+
+      if (noSwap) break;
+    }
+  }
+
+  return arr;
+}
+
 //! Selection Sort
 
 function selectionSort(arr) {
