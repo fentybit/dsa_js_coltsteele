12cf823499c7dce9c1662b8ec797d9b5380caf27
fentybit <fentymhall@gmail.com>
2021-12-24 14:02:19 -0800

    findPair

--- 01_problem_solving_patterns.js
+++ 01_problem_solving_patterns.js
@@ -311,6 +311,41 @@ function isSubsequence(str1, str2) {
   return isSubsequence(str1, str2.slice(1));
 }
 
+// Given an unsorted array and a number n, find if there exists a pair of elements in the array whose 
+// difference is n. This function should return true if the pair exists or false if it does not.
+// Time Complexity O(n log n) and Space Complexity O(1)
+
+findPair([6, 1, 4, 10, 2, 4], 2) // true
+findPair([6, 1, 4, 10, 2, 4], 22) // false
+findPair([8, 6, 2, 4, 1, 0, 2, 5, 13], 1) // true
+findPair([4, -2, 3, 10], -6) // true
+findPair([], 0) // false
+findPair([5, 5], 0) // true
+findPair([-4, 4], -8) // true
+findPair([-4, 4], 8) // true
+
+function findPair(arr, num) {
+  if (arr.length === 0) return false;
+  arr.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 1;
+
+  while (i < arr.length && j < arr.length) {
+    let subtract = arr[j] - arr[i];
+
+    if (i !== j && Math.abs(subtract) === Math.abs(num)) {
+      return true;
+    } else if (subtract < num) {
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return false;
+}
+
 //! Sliding Window
 
 // Write a function called maxSubarraySum which accepts an array of integers and a number called n. 
