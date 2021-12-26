ed60ff1785e5aed13114b8ef468f825a4c1538ac
fentybit <fentymhall@gmail.com>
2021-12-26 14:01:53 -0800

    bst on removeNode

--- 08_binary_search_trees.js
+++ 08_binary_search_trees.js
@@ -55,6 +55,42 @@ class BinarySearchTree {
 
     return false;
   }
+
+  // Implement the following function on the BinarySearchTree.prototype.
+  // This function should remove a node from a binary search tree. Your remove
+  // function should be able to handle removal of the root node, removal of a node
+  // with one child and removal of a node with two children. The function should
+  // return the node removed.
+  remove(value) {
+    let foundNode = this.find(value);
+
+    // helper function to remove node value in the tree and behaves recursively
+    const removeNode = (node, value) => {
+      if (!node) return null;
+
+      if (value === node.value) {
+        if (!node.left && !node.right) return null;
+        if (!node.left) return node.right;
+        if (!node.right) return node.left;
+
+        let temp = node.right;
+        while (temp.left) {
+          temp = temp.left;
+        }
+        node.value = temp.value;
+        node.right = removeNode(node.right, temp.value);
+      } else if (value < node.value) {
+        node.left = removeNode(node.left, value);
+      } else {
+        node.right = removeNode(node.right, value);
+      }
+
+      return node;
+    }
+
+    this.root = removeNode(this.root, value);
+    return foundNode ? value : undefined;
+  }
 }
 
 // Both insertion and searching time complexity is O(log n) on best and average
