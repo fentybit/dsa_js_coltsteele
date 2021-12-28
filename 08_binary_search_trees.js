6676bd76bc31603e193e9fe1692dcdde0ecb8688
fentybit <fentymhall@gmail.com>
2021-12-28 14:47:23 -0800

    bst on isBalanced

--- 08_binary_search_trees.js
+++ 08_binary_search_trees.js
@@ -96,7 +96,57 @@ class BinarySearchTree {
     if (!this.root) return undefined;
     return this.root.value;
   }
+
+  // Write a function on the BinarySearchTree class.
+  // isBalanced returns true if the BST is balanced, otherwise returns false.
+  // A balanced tree is defined as a tree where the depth of all leaf nodes or
+  // nodes with single children differ by no more than one.
+  isBalanced() {
+    let leftCounter = 0, rightCounter = 0;
+
+    let leftNode = this.root;
+    while (leftNode.left) {
+      leftNode = leftNode.left;
+      leftCounter++;
+    }
+
+    let rightNode = this.root;
+    while (rightNode.right) {
+      rightNode = rightNode.right;
+      rightCounter++;
+    }
+
+    return Math.abs(rightCounter - leftCounter) <= 1;
+  }
 }
+
+let bst = new BinarySearchTree();
+bst
+    .insert(15)
+    .insert(20)
+    .insert(10)
+    .insert(12)
+    .insert(1)
+    .insert(5)
+    .insert(50)
+    .insert(60)
+    .insert(30)
+    .insert(25)
+    .insert(23)
+    .insert(24)
+    .insert(70)
+bst.remove(10);
+bst.root.left.value // 12
+bst.root.left.left.value // 1
+bst.root.left.left.right.value // 5
+
+let bst2 = new BinarySearchTree()
+bst2.insert(5)
+bst2.isBalanced() // true
+bst2.insert(6)
+bst2.isBalanced() // true
+bst2.insert(7)
+bst2.isBalanced() // false
 
 // Both insertion and searching time complexity is O(log n) on best and average
 // This means if you double the number of nodes, you only increase the number
