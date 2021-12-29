51b95923f836e05d19a21a311849ff777508ded4
fentybit <fentymhall@gmail.com>
2021-12-29 10:06:42 -0800

    update MaxBinaryHeap bubbleUp method

--- 10_binary_heaps.js
+++ 10_binary_heaps.js
@@ -15,6 +15,7 @@ class MaxBinaryHeap {
     const element = this.values[idx];
     while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
       if (element <= parent) break;
       this.values[parentIdx] = element;
       this.values[idx] = parent;
